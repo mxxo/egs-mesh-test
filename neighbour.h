85eedd7f6d1b9fd103fbd06ecb0de6d1bde1b36a
Max Orok <maxwellorok@gmail.com>
2020-11-26 14:26:45 -0500

    Move to SharedNodes class to look up elements that share a node

--- neighbour.h
+++ neighbour.h
@@ -45,6 +45,12 @@ public:
         _c = sorted[2];
         _d = sorted[3];
     }
+    std::array<int, 4> nodes() const {
+        return std::array<int, 4> {_a, _b, _c, _d};
+    }
+    int max_node() const {
+        return _d;
+    }
     std::array<Face, 4> faces() const {
         return {
             std::array<int, 3>{_b, _c, _d},
@@ -55,25 +61,11 @@ public:
     }
 
 private:
-    friend std::vector<int> flatten_tetrahedron_vector(const std::vector<Tetrahedron>& elements);
-
     int _a = -1;
     int _b = -1;
     int _c = -1;
     int _d = -1;
 };
-
-std::vector<int> flatten_tetrahedron_vector(const std::vector<Tetrahedron>& elements) {
-    std::vector<int> nodes;
-    nodes.reserve(elements.size() * 4);
-    for (const auto& e: elements) {
-        nodes.push_back(e._a);
-        nodes.push_back(e._b);
-        nodes.push_back(e._c);
-        nodes.push_back(e._d);
-    }
-    return nodes;
-}
 
 inline void print_vec(const std::vector<int>& vec) {
     for (auto v: vec) {
@@ -81,11 +73,6 @@ inline void print_vec(const std::vector<int>& vec) {
     }
     std::cout << "\n";
 }
-
-struct EltsAroundPoints {
-    std::vector<int> elt_list;
-    std::vector<int> list_indices;
-};
 
 /// Get the sparse-dense node renumbering map.
 std::unordered_map<int, int> renumber_sparse_nodes(const std::vector<int>& nodes) {
@@ -103,97 +90,72 @@ std::unordered_map<int, int> renumber_sparse_nodes(const std::vector<int>& nodes
     return node_renum;
 }
 
+class SharedNodes {
+public:
+    SharedNodes(std::vector<std::vector<int>> shared_nodes) :
+        shared_nodes(std::move(shared_nodes)) {}
+    const std::vector<int>& elements_around_node(int node) const {
+        return shared_nodes.at(node - 1);
+    }
+private:
+    std::vector<std::vector<int>> shared_nodes;
+};
+
 // Find the elements around each node.
-// Returns a list of elements and a list of indices into the elements.
 //
 // Adapted from Applied CFD Techniques section 2.2.1
-EltsAroundPoints elements_around_points(const std::vector<int>& nodes) {
-    constexpr int NODES_PER_ELT = 4;
-    assert(nodes.size() % NODES_PER_ELT == 0);
+SharedNodes elements_around_nodes(const std::vector<mesh_neighbours::Tetrahedron>& elements) {
     // the number of unique nodes is equal to the maximum node number
     // because the nodes are continuously numbered from 1..=max_node
-    int num_unique_nodes = *std::max_element(nodes.begin(), nodes.end());
-    // fill indices vector with 0
-	std::vector<int> indices(num_unique_nodes + 1, 0);
-
-    int num_elts = nodes.size() / NODES_PER_ELT;
-    std::cout << "num elts:" << num_elts << "\n";
-
-    // counts up the number of times a number idx node is used
-    for (int i = 0; i < num_elts; ++i){
-      for (int j = 0; j < NODES_PER_ELT; ++j){
-          auto idx = nodes.at(i * NODES_PER_ELT + j);
-          indices.at(idx) += 1;
-      }
-    }
-
-    for (std::size_t i = 1; i < indices.size(); ++i){
-      indices.at(i) += indices.at(i-1);
-    }
-
-    std::vector<int> eltList(indices.back(), -1);
-    for (int i = 0; i < num_elts; ++i){
-        for (int j = 0; j < NODES_PER_ELT; ++j){
-            auto node = nodes[i * NODES_PER_ELT + j] - 1;
-            auto node_pos = indices.at(node);
-            eltList[node_pos] = i;
-            indices.at(node) += 1;
+    int max_node = -1;
+    for (const auto& elt: elements) {
+        if (elt.max_node() > max_node) {
+            max_node = elt.max_node();
         }
     }
 
-    // shift indices vector right by 1
-    for (std::size_t i = indices.size()-1; i > 0; --i){
-        indices[i] = indices[i-1];
+    std::vector<std::vector<int>> shared_nodes(max_node);
+    for (std::size_t i = 0; i < elements.size(); i++) {
+        for (auto node: elements[i].nodes()) {
+            shared_nodes.at(node - 1).push_back(i);
+        }
     }
-    indices[0] = 0;
-
-	return EltsAroundPoints { eltList, indices };
+    return SharedNodes(shared_nodes);
 }
 
 // Given a list of node numbers starting from 1, returns the element neighbours.
 //
 // Adapted from Applied CFD Techniques section 2.2.3
 std::vector<int> tetrahedron_neighbours(const std::vector<mesh_neighbours::Tetrahedron>& elements) {
-    constexpr int NODES_PER_ELT = 4;
-    constexpr int FACES_PER_ELT = 4;
-    constexpr int NODES_PER_FACE = 3;
-    auto element_nodes = flatten_tetrahedron_vector(elements);
+    const int NUM_FACES = 4;
+    const auto shared_nodes = elements_around_nodes(elements);
 
-    // this implementation requires one less node per face than the number of element nodes
-    assert(NODES_PER_FACE == NODES_PER_ELT - 1);
-
-    const auto elt_indices = elements_around_points(element_nodes);
-    const auto& eltList = elt_indices.elt_list;
-    const auto& indices = elt_indices.list_indices;
-
-    const int num_elts = element_nodes.size() / NODES_PER_ELT;
     // initialize neighbour element index vector with "no neighbour" constant
-    std::vector<int> neighbours(num_elts * FACES_PER_ELT, mesh_neighbours::NONE);
+    std::vector<int> neighbours(elements.size() * NUM_FACES, mesh_neighbours::NONE);
 
-    for (int i = 0; i < num_elts; i++) {
+    for (int i = 0; i < static_cast<int>(elements.size()); i++) {
         auto elt_faces = elements[i].faces();
-        for (int j = 0; j < FACES_PER_ELT; j++) {
+        for (int f = 0; f < NUM_FACES; f++) {
             // if this face's neighbour was already found, skip it
-            if (neighbours[i * FACES_PER_ELT + j] != NONE) {
+            if (neighbours[i * NUM_FACES + f] != NONE) {
                 continue;
             }
-            auto face = elt_faces[j];
-            // select a point of this face and loop over the other elements that share it
-            // -- any point will work, since to be a neighbour other elements
-            //    must have the same list of face points
-            auto face_point = face[0];
-            for (int elt_idx = indices[face_point - 1]; elt_idx < indices[face_point]; elt_idx++) {
-                auto elt = eltList.at(elt_idx);
-                if (elt == i) {
+            auto face = elt_faces[f];
+            // select a face node and loop through the other elements that share it
+            // -- any node will work, since to be a neighbour other elements
+            //    must have the same list of face nodes
+            auto face_node = face[0];
+            const auto& elts_sharing_node = shared_nodes.elements_around_node(face_node);
+            for (auto j: elts_sharing_node) {
+                if (j == i) {
                     // elt can't be a neighbour of itself, skip it
                     continue;
                 }
-                // to match, all face nodes must match the reference face nodes
-                auto other_elt_faces = elements[elt].faces();
-                for (int other_face_idx = 0; other_face_idx < FACES_PER_ELT; other_face_idx++) {
-                    if (face == other_elt_faces[other_face_idx]) {
-                        neighbours[i * FACES_PER_ELT + j] = elt;
-                        neighbours[elt * FACES_PER_ELT + other_face_idx] = i;
+                auto other_elt_faces = elements[j].faces();
+                for (int jf = 0; jf < NUM_FACES; jf++) {
+                    if (face == other_elt_faces[jf]) {
+                        neighbours[i * NUM_FACES + f] = j;
+                        neighbours[j * NUM_FACES + jf] = i;
                         break;
                     }
                 }
